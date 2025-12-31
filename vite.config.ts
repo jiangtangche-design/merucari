621ba9fe283c401d9caa758c7b9f3d5e4d7def67
jiangtangche-design <jiangtangche@gmail.com>
2025-12-31 11:49:21 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
   },
   server: {
-    port: 3000
+    port: 3000,
+    host: true
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
   }
 });
